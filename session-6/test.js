057a463274ad2735164160c455156b5b8adf02a0
Sabeel Khan <sabeelhps@gmail.com>
2020-11-22 23:31:25 +0530

    session-7

---
+++ session-6/test.js
@@ -0,0 +1,29 @@
+// Exercise 1
+
+let num=1;
+
+while(num<=10){
+    console.log(num);
+    num+=2;
+}
+
+
+// Exercise 2
+
+let num=1;
+
+while(num<=20){
+    if(num%4===0){
+        console.log(num);
+    }
+    num++;
+}
+
+// Exercise 3
+
+let num=100;
+
+while(num<150){
+    console.log(num+1);
+    num--;
+}
---
+++ session-6/while.js
@@ -0,0 +1,42 @@
+
+
+
+// console.log(1);
+// console.log(2);
+// console.log(3);
+// console.log(4);
+// console.log(5);
+// console.log(6);
+// console.log(7);
+// console.log(8);
+// console.log(9);
+// console.log(10);
+
+
+
+// While Loops
+
+// syntax
+// while(someCondition){
+
+// }
+
+// let num=1;//Initialisation
+
+// while(num<=5){//condition
+//     console.log(num);
+//     num++;//Updation
+// }
+
+let x=10;
+
+// Infinite loops
+
+while(-100){
+    console.log(x);
+    x--;
+}
+
+
+
+
