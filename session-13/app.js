8c9905be143c36385b5bb2df66b76f978c91a05a
Sabeel Khan <sabeelhps@gmail.com>
2021-01-23 21:32:16 +0530

    session-13

---
+++ session-13/app.js
@@ -0,0 +1,69 @@
+
+// function fun(x,y){
+
+//     return x+y;
+// }
+
+// console.log(fun(2,3));
+
+
+// console.log(add(2,3));
+
+// var add = function(x,y){
+//     return x+y;
+// }
+
+add();
+
+var add=function(){
+    console.log("Hello!!");
+}
+
+
+sum();
+
+function sum(){
+
+}
+
+
+
+
+
+
+// var x=10;
+
+// function fun(){
+//     x++;
+// }
+
+// fun();
+// console.log(x);
+
+
+// var y=10;
+
+// function f(){
+
+//     var z=5;
+
+//     z++;
+//     y++;
+
+//     console.log(z);
+// }
+
+// f();
+// console.log(y);
+
+// console.log(x);
+
+// fun();
+
+// var x=10;
+
+// function fun(){
+//     console.log("Hello World");
+// }
+
+// console.log(x);
