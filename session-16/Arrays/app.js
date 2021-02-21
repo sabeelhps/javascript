118927b96be794d07c1eab9d73cf11ee25d0645b
Sabeel Khan <sabeelhps@gmail.com>
2021-02-21 19:57:30 +0530

    session-16

---
+++ session-16/Arrays/app.js
@@ -0,0 +1,47 @@
+// Arrays
+
+// let arr = ["Soham", "Vihaan", "Sabeel","sdhjhkf","dshjhjs",true,1.3456,3266,undefined,null]; //Arrays are hetrogenous
+
+// for (let i = 0; i < arr.length; i++){
+//     console.log(arr[i]);
+// }
+
+// Arrays methods/Function
+
+// shift
+let colors = ['red','green','blue','yellow','orange'];
+// let ans = colors.shift();
+// console.log(ans);
+
+// unshift
+
+// colors.unshift('purple','Magenta');
+
+// console.log(colors);
+
+
+// push
+
+// colors.push('Aqua','White');
+// console.log(colors);
+
+// pop
+
+// let ans = colors.pop();
+
+// console.log(ans);
+// console.log(colors);
+
+// let str = "This is JS Class";
+
+// let ans = str.split(' ');
+
+// console.log(ans[0]);
+
+let arr = ['This', 'is', 'js', 'class'];
+
+let ans = arr.join('-');
+console.log(ans);
+
+
+
---
+++ session-16/Strings/app.js
@@ -0,0 +1,50 @@
+
+// METHODS WITH NO ARGUMENTS------------------
+
+
+// let str = "Hello World";
+
+// let ans = str.toUpperCase(); //example of non destructive function
+
+// console.log(ans);
+// console.log(str);
+
+// let msg = "          Leave me Alone I hate u plzz     ";
+
+// let angryMsg = msg.trim().toLowerCase();
+
+// console.log(angryMsg);
+
+
+// METHODS WITH ARGUMENTS------------------------------
+
+// let str = "Hello World";
+
+// let ans = str.substr(-7,5);
+
+// let ans = str.slice(-2,-5);
+
+// let index = str.indexOf('World',3); //used to search in a string
+
+// console.log(index);
+
+// let msg = "cat bat rat cat mat bat sat cat";
+
+// let replacedMsg = str.replaceAll('l', 'x');
+
+// let newMsg = msg.replaceAll('cat', 'DOG');
+// console.log(newMsg);
+
+// console.log(replacedMsg);
+
+// let str = "Hello";
+
+// let ans = str.repeat(3);
+
+// console.log(ans);
+
+
+
+
+
+
